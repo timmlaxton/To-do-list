3d002fdef041fbc13a4bdefeb1d30590fc4a7846
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-05-25 08:44:47 +0100

    can delete itmes

--- todos/app.js
+++ todos/app.js
@@ -1,5 +1,6 @@
 const addForm  = document.querySelector('.add');
 const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 
 const generateTemplate = todo => {
 
@@ -32,3 +33,22 @@ if(todo.length){
 
 
 });
+
+list.addEventListener('click', e => {
+
+if(e.target.classList.contains('delete')){
+    e.target.parentElement.remove();
+}
+});
+
+const filterTodos  = (term) => {
+Array.from(list.children)
+.filter((todo) => !todo.textContent.includes(term))
+.forEach((todo) => todo.classList.add('filtered'))
+    
+};
+
+search.addEventListener('keyup', () => {
+    const term = search.value.trim();
+    filterTodos(term)
+})
